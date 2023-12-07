ef262939ca25174e5a49e0df2b1af7c00fa61cb4
valnhe <vpazcastle@gmail.com>
2023-12-07 01:00:31 -0300

    more things

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,15 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				bkg: 'rgb(var(--color-bkg) / <alpha-value>)',
+				primary: 'rgb(var(--color-primary) / <alpha-value>)',
+				secundary: 'rgb(var(--color-secundary) / <alpha-value>)',
+				tertiary: 'rgb(var(--color-tertiary) / <alpha-value>)',
+				quaternary: 'rgb(var(--color-quaternary) / <alpha-value>)',
+			}
+		},
 	},
 	plugins: [],
 }

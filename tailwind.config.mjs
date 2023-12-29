a2d9c178dec56501e6bb4a59658ebe33b77eccc3
valnhe <vpazcastle@gmail.com>
2023-12-28 23:20:36 -0300

    Update color variables in Layout.astro and tailwind.config.mjs

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -6,14 +6,9 @@ export default {
 		extend: {
 			colors: {
 				bkg: 'rgb(var(--color-bkg) / <alpha-value>)',
-				aboutmecolor: 'rgb(var(--color-aboutmecolor) / <alpha-value>)',
-				curriculumcolor: 'rgb(var(--color-curriculumcolor) / <alpha-value>)',
-				discordcolor: 'rgb(var(--color-discordcolor) / <alpha-value>)',
-				tecnocolor: 'rgb(var(--color-tecnocolor) / <alpha-value>)',
-				changecolor: 'rgb(var(--color-changecolor) / <alpha-value>)',
-				hourcolor: 'rgb(var(--color-hourcolor) / <alpha-value>)',
-				contact: 'rgb(var(--color-contact) / <alpha-value>)',
-				buttoncontact:'rgb(var(--color-buttoncontact) / <alpha-value>)',
+				primarycolor: 'rgb(var(--color-primarycolor) / <alpha-value>)',
+				secondarycolor: 'rgb(var(--color-secondarycolor) / <alpha-value>)',
+				ternarycolor: 'rgb(var(--color-ternarycolor) / <alpha-value>)'
 
 			},
 			gridTemplateColumns: {
